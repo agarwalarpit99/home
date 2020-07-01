84f4611a675e814ff81c933605f3d026e07c5df3
agarwalarpit99 <arpitagarwal990@gmail.com>
2020-07-01 11:02:17 +0530

    wdw

--- school/urls.py
+++ school/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns=[
-	path('home',views.home,name="home"),
+	path('',views.home,name="home"),
 	path('about',views.about,name="about"),
 	path('admissions',views.admissions,name='admissions'),
 	path('contact',views.contact,name='contact'),
